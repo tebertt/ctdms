44bf71ab46e983f1a82121f9a61cb26ff2055df4
Troy Ebert <troy.ebert@outlook.com>
2026-04-06 22:14:17 -0600

    Removed cmake artifacts

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/troy/Documents/ctdms
-# Build directory: /Users/troy/Documents/ctdms/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_ctdms "/Users/troy/Documents/ctdms/build/test_ctdms")
-set_tests_properties(test_ctdms PROPERTIES  _BACKTRACE_TRIPLES "/Users/troy/Documents/ctdms/CMakeLists.txt;65;add_test;/Users/troy/Documents/ctdms/CMakeLists.txt;0;")
